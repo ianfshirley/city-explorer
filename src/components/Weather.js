28c807f15c92356f1b1f5a29c5b79aa64014d443
ianfshirley <ianfshirley88@gmail.com>
2022-11-18 16:30:32 -0800

    weather info displaying properly

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,18 +7,21 @@ export default class Weather extends React.Component {
     // console.log('important',info['app_temp']);
     return (
       <>
-        <h3>Current Weather</h3>
-        <div>
-          {this.props.forecast.map((day, idx) => (
-            <div key={idx}>
-              <p>Date: {day.date}</p>
-              <p>high: {day.high}</p>
-              <p>low: {day.low}</p>
-              <p>desc: {day.description}</p>
-            </div>
-          ))
-          }
-        </div>
+
+        {this.props.forecast.length < 1 ? <p></p> :
+          <div>
+            <p>3 Day Forecast</p>
+            {this.props.forecast.map((day, idx) => (
+              <div key={idx}>
+                <p>Date: {day.date}</p>
+                <p>
+                  Low of {day.low}°C, high of {day.high} with {day.description}
+                </p>
+              </div>
+            ))
+            }
+          </div>
+        }
       </>
     )
   }
