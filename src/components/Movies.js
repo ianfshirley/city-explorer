fca1bd40abcf3ff2db4f4fa246e730b0b97d7837
ianfshirley <ianfshirley88@gmail.com>
2022-11-19 18:23:27 -0800

    created movie component, confirmed it works on local server

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Movie from './Movie';
 
 export default class Movies extends React.Component {
 
@@ -6,15 +7,10 @@ export default class Movies extends React.Component {
     console.log('topTenMovies: ', this.props.topTenMovies);
     return (
       <>
-        {this.props.topTenMovies.length < 1 ? <p></p> : 
+        {this.props.topTenMovies.length < 1 ? <p></p> :
           <div className='topTenMovies'>
             {this.props.topTenMovies.map((movie, idx) => (
-              <div key={idx}>
-                <p>Movie: {movie.title}</p>
-                <p>Release Date: {movie.releaseDate}</p>
-                <p>Overview: {movie.overview}</p>
-                <img src={movie.url} alt={movie.title} title={movie.title}/>
-              </div>
+              <Movie oneMovie={movie} key={idx} />
             ))}
           </div>
         }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,14 +8,12 @@ export default class Weather extends React.Component {
     // console.log('important',info['app_temp']);
     return (
       <>
-
         {this.props.forecast.length < 1 ? <p></p> :
           <div>
             <p>3 Day Forecast</p>
             {this.props.forecast.map((day, idx) => (
-              <WeatherDay dailyForecast={day} key={idx}/>
-              ))
-            }
+              <WeatherDay dailyForecast={day} key={idx} />
+            ))}
           </div>
         }
       </>
