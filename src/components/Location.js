833cd844bfff8939c014d5e9a18b457d5c289ed8
ianfshirley <ianfshirley88@gmail.com>
2022-11-20 21:43:53 -0800

    refactored code, deleted console logs

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class Location extends React.Component {
   render() {
-    // let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`;
     return(
       <>
         <div className='mapInfo'>
