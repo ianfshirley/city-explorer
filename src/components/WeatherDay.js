aa7388782d00c4d1e0b8aae67dda883461f6a42d
ianfshirley <ianfshirley88@gmail.com>
2022-12-07 19:58:14 -0800

    put movies into carousel, used CSS grid for page layout

--- src/components/WeatherDay.js
+++ src/components/WeatherDay.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class WeatherDay extends React.Component {
   render() {
     return (
-      <div key={this.props.idx}>
+      <div className='weatherDay' key={this.props.idx}>
         <p>Date: {this.props.dailyForecast.date}</p>
         <p>
           Low of {this.props.dailyForecast.low}°C, high of {this.props.dailyForecast.high} with {this.props.dailyForecast.description}
